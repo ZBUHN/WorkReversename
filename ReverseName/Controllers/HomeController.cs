79ef3304fab8251e9609c545bff6b3e88ee2204f
Zabnullah <z7777075995@gmail.com>
2023-05-18 14:46:15 +0300

    Final Commit

---
+++ ReverseName/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReverseName.Models;
+using System.Diagnostics;
+
+namespace ReverseName.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ReverseN(string Name)
+        {
+            var reverse = new string(Name.Reverse().ToArray());
+            ViewData["RN"] = reverse;
+            return View("Index");
+        }
+    }
+}
